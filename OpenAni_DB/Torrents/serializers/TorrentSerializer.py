49ef8b89e3caea5efad29f69f82417e5e883639c
rayden <raydenldon@gmail.com>
2026-02-17 19:20:58 +0100

    cosas añadidads a openAni_db_viejo

---
+++ OpenAni_DB/Torrents/serializers/TorrentSerializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+# from
+
+class TorrentSerializer(serializers.ModelSerializer):
+    class Meta:
+        # model =
+        # model = TorrentSerializer
+        fields = "__all__"
