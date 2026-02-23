3b77d55da2249630c3d6cb7c0974efc9c161dd71
StoianAlejandro <s.alejandrostoian@gmail.com>
2026-02-23 11:02:39 +0100

    Empezado el reset de contraseña

--- OpenAni_DB/OpenAni_DB/settings.py
+++ OpenAni_DB/OpenAni_DB/settings.py
@@ -120,15 +120,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+
 }
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1), # El token dura 1 día
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'AUTH_HEADER_TYPES': ('Bearer ', ), # En Android enviarás: "Bearer <token>"
+    'AUTH_HEADER_TYPES': ('Bearer', ), # En Android enviarás: "Bearer <token>"
 }
 
 # Internationalization
