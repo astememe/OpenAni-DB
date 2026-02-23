51a8abdaa6e788d33f29f940db873e505ca36e0d
rayden <raydenldon@gmail.com>
2026-02-23 18:23:21 +0100

    meliminado categoria de favoritos

--- OpenAni_DB/Users/serializers/favorito_serializer.py
+++ OpenAni_DB/Users/serializers/favorito_serializer.py
@@ -6,7 +6,6 @@ from Users.models import FavoritoModel
 class FavoritoSerializer(serializers.ModelSerializer):
     nombre_usuario = serializers.CharField(required=True)
     nombre_torrent = serializers.CharField(required=True)
-    categoria = serializers.CharField(required=True)
     enlace = serializers.CharField(required=True)
     tamano = serializers.CharField(required=True)
     fecha = serializers.CharField(required=True)
@@ -15,4 +14,4 @@ class FavoritoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = FavoritoModel
-        fields = ("nombre_usuario", "nombre_torrent", "categoria", "enlace", "tamano", "fecha", "seeders", "leechers")
+        fields = ("nombre_usuario", "nombre_torrent", "enlace", "tamano", "fecha", "seeders", "leechers")
