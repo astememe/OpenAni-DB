c14d4273d71dbe086a9fd306d300b5232b663996
StoianAlejandro <s.alejandrostoian@gmail.com>
2026-02-23 11:23:07 +0100

    Confirm reset empeado

--- OpenAni_DB/Users/views/confirm_password_reset_view.py
+++ OpenAni_DB/Users/views/confirm_password_reset_view.py
@@ -1,16 +1,24 @@
 import random
 
+from rest_framework import status
 from rest_framework.authtoken.admin import User
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.views import APIView
-from Users.models import ResetPassword
+from Users.models import ResetPassword, UserModel
+
 
 class ConfirmPasswordResetView(APIView):
-    permission_classes = [AllowAny]
-    def post(self, request):
-        email = request.data['email']
-        user = User.objects.filter(email=email).first()
+    permission_classes = (AllowAny,)
 
-        if user:
-            code = str(random.randint(100000, 999999))
-            ResetPassword.objects.create(user=user, code=code)
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        reset_code = request.data.get('reset_code')
+
+        reset_entry = ResetPassword.objects.filter(user__email=email, code=reset_code)
+
+        if reset_entry:
+            user = UserModel.objects.get(email=email)
+            user.set_password(password)
+
