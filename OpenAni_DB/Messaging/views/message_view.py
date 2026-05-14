538a29dcad909c5d0bad1b9cc02b47487072560c
StoianAlejandro <s.alejandrostoian@gmail.com>
2026-05-14 19:00:07 +0200

    Ahora se pueden ver los mensajes de una habitación

--- OpenAni_DB/Messaging/views/message_view.py
+++ OpenAni_DB/Messaging/views/message_view.py
@@ -1,12 +1,26 @@
+from django.db.models import Q
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from Messaging.models import MessageModel, RoomModel
 from Messaging.serializers import MessageSerializer
 
+
 class MessageListView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        thread_id = request.query_params.get('group_id')
-        messages = MessageModel.objects.filter(room_id=thread_id)
+        room_id = request.query_params.get('room_id')
+
+        room = RoomModel.objects.filter(
+            Q(id=room_id) & (Q(user1=request.user) | Q(user2=request.user))
+        ).first()
+
+        if not room:
+            return Response({"error": "No tienes permiso o la sala no existe"}, status=status.HTTP_403_FORBIDDEN)
+
+        messages = MessageModel.objects.filter(room=room)
         serializer = MessageSerializer(messages, many=True)
 
         return Response({"messages": serializer.data})
