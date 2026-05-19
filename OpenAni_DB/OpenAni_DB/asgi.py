006d7ab85562eacd90823b079c41e3ee1cd1cf31
StoianAlejandro <s.alejandrostoian@gmail.com>
2026-05-19 18:35:31 +0200

    Corrregidos detalles

--- OpenAni_DB/OpenAni_DB/asgi.py
+++ OpenAni_DB/OpenAni_DB/asgi.py
@@ -9,17 +9,13 @@ from Messaging.chat_consumers import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OpenAni_DB.settings')
 
-# Aplicación ASGI de Django
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    # Maneja el HTTP normal (tus interfaces de Retrofit)
     "http": django_asgi_app,
 
-    # Maneja los WebSockets de tu App Android
     "websocket": AuthMiddlewareStack(
         URLRouter(
-
             routing.websocket_urlpatterns
         )
     ),
